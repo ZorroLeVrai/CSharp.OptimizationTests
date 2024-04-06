94af50943283f8fe989965a953c9d80af045f683
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-04-06 17:42:33 +0200

    Update the parallel degree

--- BenchmarkConsole/Exercices/SumInverseBenchmark.cs
+++ BenchmarkConsole/Exercices/SumInverseBenchmark.cs
@@ -28,13 +28,13 @@ public class SumInverseBenchmark
     [Benchmark]
     public void ParallelWithMaxDegreeWithLock()
     {
-        computer.ParallelWithMaxDegreeWithLock(NB_TERME, 100);
+        computer.ParallelWithMaxDegreeWithLock(NB_TERME, 1000);
     }
 
     [Benchmark]
     public void ParallelWithMaxDegreeWithSpinLock()
     {
-        computer.ParallelWithMaxDegreeWithSpinLock(NB_TERME, 100);
+        computer.ParallelWithMaxDegreeWithSpinLock(NB_TERME, 1000);
     }
 
     [Benchmark]
