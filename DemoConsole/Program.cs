c7a6e779f6e3d9f91126bf8438c014bd9b573d63
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-11-13 16:52:21 +0100

    Update Excercises

--- DemoConsole/Program.cs
+++ DemoConsole/Program.cs
@@ -1,13 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DemoConsole;
 
-//var guid = Guid.NewGuid();
 
-var guid = new Guid("3031f416-4eae-402b-8b0c-413968fc33c3");
-
-var str = Convert.ToBase64String(guid.ToByteArray());
-    //.Replace('/', '-')
-    //.Replace('+', '_')
-    //.Substring(0, 22);
-Console.WriteLine(guid);
-Console.WriteLine(str);
+var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Comparateur>();
