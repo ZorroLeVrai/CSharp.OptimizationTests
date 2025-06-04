9f7c135b371d7763aa0c4476c6db307b4694a9ca
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2025-06-04 07:58:59 +0200

    Update ConverterService.cs

--- DistantWebApp/Services/ConverterService.cs
+++ DistantWebApp/Services/ConverterService.cs
@@ -4,7 +4,7 @@ public static class ConverterService
 {
     public static string IntToBinary(int value)
     {
-        //await Task.Delay(1000);
+        //Task.Delay(1000);
         Thread.Sleep(1000);
         return Convert.ToString(value, 2);
     }
